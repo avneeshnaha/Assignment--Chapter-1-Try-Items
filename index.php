4d35b1ad0d2cdd8b17a7b5b6bb016e9e0428e6db
avneeshnaha <nahaavneesh@gmail.com>
2023-09-12 17:00:50 -0700

    made changes exercise 2

--- index.php
+++ index.php
@@ -15,3 +15,21 @@ $price = 2
         $<?php echo $price; ?> per pack. </p>
 </body>
 </html>
+<?php
+$name = 'Guest';
+$name = 'Avneesh';
+$price = 5;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Updating variables</title>
+        <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Welcome<?php echo $name; ?></h2>
+    <p> The cost of your candu is
+        $<?php echo $price; ?>per pack.</p>
+</body>
+</html>
