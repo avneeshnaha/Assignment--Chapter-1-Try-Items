6c185c76fb0b07935bd7da575eeef56ba237da9c
avneeshnaha <nahaavneesh@gmail.com>
2023-09-12 17:50:53 -0700

    added offer and price

--- index.php
+++ index.php
@@ -98,6 +98,33 @@ $nutrition['protein']= 7.3;
         <p>Fiber:   <?php echo $nutrition['Fiber']; ?>%<p>
 </body>
 </html>
+<?php
+$offers = [
+    ['name' => 'Toffee', 'price' => 5, 'stock' => 120,],
+    ['name' => 'Mints', 'price' => 3, 'stock' => 66,],
+    ['name' => 'Fudge', 'price' => 4, 'stock' => 97,],
+];
+?>
+<!DOCTYPE html>
+<html>
+    <head> . . . </head>
+    <body>
+        <h1>The Candy Store</h1>
+        <h2>Offers</h2>
+        <p><?php echo $offers[0] ['name']; ?> - 
+        $<?php echo $offers[0] ['price']; ?> </p>
+        <p><?php echo $offers[1] ['name']; ?> - 
+        $<?php echo $offers[1] ['price']; ?> </p>
+        <p><?php echo $offers[2] ['name']; ?> - 
+        $<?php echo $offers[2] ['price']; ?> </p>
+</body>
+</html>
+
+
+
+
+
+
 
 
 
