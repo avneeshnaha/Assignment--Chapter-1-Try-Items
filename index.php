9bf58515203913051bec13fd538b4d6fb6bd4eae
avneeshnaha <nahaavneesh@gmail.com>
2023-09-12 17:24:16 -0700

    added licorice

--- index.php
+++ index.php
@@ -54,3 +54,28 @@ $nutrition = [
         <p>Protein:  <?php echo $nutrition['Protein']; ?>%</p>
 </body>
 </html>
+<?php
+$best_sellers = ['chocolate', 'Mints', 'Fudge','Licorice', 'Bubble Gum', 'Toffee', 'Jelly beans',];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Updating Variables</title>
+        <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2>Best Sellers</h2>
+    <ul>
+        <li><?php echo $best_sellers[0]; ?></li>
+        <li><?php echo $best_sellers[1]; ?></li>
+        <li><?php echo $best_sellers[2]; ?></li>
+        <li><?php echo $best_sellers[3]; ?></li>
+        <li><?php echo $best_sellers[4]; ?></li>
+
+</ul>
+</body>
+</html>
+
+
+
