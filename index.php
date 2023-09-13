a18b0e3037f4018e3454b73174225c21afbd7534
avneeshnaha <nahaavneesh@gmail.com>
2023-09-12 18:01:04 -0700

    added my name

--- index.php
+++ index.php
@@ -162,6 +162,23 @@ $total    = $subtotal + $tax;
     <p>Total: $<?= $total ?></p>
   </body>
 </html>
+<?php 
+$prefix  = 'Thank you';
+$name    = 'Avneesh';
+$message = $prefix . ', ' . $name;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>String Operator</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2><?= $name ?>'s Order</h2>
+    <p><?= $message ?></p>
+  </body>
+</html>
 
 
 
