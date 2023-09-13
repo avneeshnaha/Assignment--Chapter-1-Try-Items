bc6cdfcf71221862b573723103dc2e79162cdd68
avneeshnaha <nahaavneesh@gmail.com>
2023-09-12 17:57:31 -0700

    added my name and favorite candy

--- index.php
+++ index.php
@@ -122,6 +122,23 @@ $offers = [
         $<?php echo $offers[3] ['price']; ?> </p>
 </body>
 </html>
+<?php 
+$name      = 'Avneesh';
+$favorites = ['Chocolate', 'Toffee', 'Fudge' 'Licorice',];
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Echo Shorthand</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2>Welcome <?= $name ?></h2>
+    <p>Your favorite type of candy is:
+       <?= $favorites[0] ?>.</p>
+  </body>
+</html>
 
 
 
